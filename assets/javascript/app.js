14d83bfae05c78695ee6b4627cae32ccde142ebc
reubenwalker64 <rwalker@mobile-atom.com>
2019-01-06 15:00:56 -0500

    2nd commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+//Button on click event shows questions
+
+//Timer starts running
+
+//Button on click events for answers record answers
+//Only one answer can be selected
+
+
+//All Done Button shows results
+
+//Timer runs out and results are displayed
+
+}
